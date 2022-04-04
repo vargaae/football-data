eb656e1dbdf53b10eb5cb6bf08b1c3b36fb78dad
Andras Varga <vargaae@hotmail.com>
2022-04-04 03:21:23 +0200

    applying ApiSlice-getCompetitions in Competitions

--- src/components/Competitions.jsx
+++ src/components/Competitions.jsx
@@ -1,5 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
 import { makeStyles } from "@mui/styles";
+import LinearProgress from "@mui/material/LinearProgress";
+import {
+  footballApi,
+  useGetCompetitionsQuery,
+} from "./../features/Api/FootballApi";
 
 const useStyles = makeStyles({
   root: {
@@ -17,6 +24,11 @@ const useStyles = makeStyles({
     paddingTop: 10,
     textAlign: "justify",
   },
+  competitionlist: {
+    padding: 32,
+    paddingBottom: 10,
+    paddingTop: 10,
+  },
   codelist: {
     lineHeight: 2,
   },
@@ -25,9 +37,65 @@ const useStyles = makeStyles({
 const Competitions = () => {
   const classes = useStyles();
 
+  const dispatch = useDispatch();
+  const {
+    data: competitions,
+    isFetching,
+    error,
+    refetch,
+  } = useGetCompetitionsQuery();
+  const [count, setCount] = useState(0);
+
+  if (error) {
+    const timer = setTimeout(() => {
+      setCount("Timeout called!");
+      console.log("fetch API Error, Refetch in 15s");
+      refetch();
+    }, 15000);
+    return () => clearTimeout(timer);
+  }
+
+  if (error)
+    return (
+      <div>
+        <button onClick={handleRefetchOne}>Force re-fetch 1</button>
+        <button onClick={handleRefetchTwo}>Force re-fetch 2</button>
+      </div>
+    );
+
+  function handleRefetchOne() {
+    // force re-fetches the data
+    refetch();
+  }
+
+  function handleRefetchTwo() {
+    // has the same effect as `refetch` for the associated query
+    dispatch(
+      footballApi.endpoints.getCompetitions.initiate(
+        { count: 5 },
+        { subscribe: false, forceRefetch: true }
+      )
+    );
+  }
+
+  if (isFetching) return <LinearProgress style={{ backgroundColor: "gold" }} />;
+
   return (
     <div className={classes.root}>
-      Competitions
+      <div className={classes.container}>
+        <h2>Available Competitions</h2>
+        {competitions?.competitions
+          .filter((league) => league?.plan === "TIER_ONE")
+          .map((league) => {
+            return (
+              <Link to={`/${league.id}`} key={league.id}>
+                <div className={classes.competitionlist}>
+                  <span>{league?.name}</span>
+                </div>
+              </Link>
+            );
+          })}
+      </div>
       <div className={classes.codelist}>
         <h2>Available Competitions Code List</h2>
         <img height="15px" width="20px" src="" /> | WC | FIFA World Cup
