51c99401447baea1f00c22d0bd3dd00283600538
Andras Varga <vargaae@hotmail.com>
2022-04-04 02:20:32 +0200

    set up Banner with some styling

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,11 +1,60 @@
 import React from "react";
 import { makeStyles } from "@mui/styles";
+import { Container, Typography } from "@mui/material";
 
-const useStyles = makeStyles(() => ({}));
+const useStyles = makeStyles(() => ({
+  banner: {
+    backgroundImage: "url(./banner.jpg)",
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "center",
+    backgroundSize: "cover",
+  },
+  bannerContent: {
+    height: 500,
+    display: "flex",
+    flexDirection: "column",
+    paddingTop: 25,
+    justifyContent: "space-around",
+  },
+  tagline: {
+    display: "flex",
+    height: "40%",
+    flexDirection: "column",
+    justifyContent: "center",
+    textAlign: "center",
+  },
+}));
 
 const Banner = () => {
   const classes = useStyles();
-  return <div>Banner</div>;
+  return (
+    <div className={classes.banner}>
+      <Container className={classes.bannerContent}>
+        <div className={classes.tagline}>
+          <Typography
+            variant="h2"
+            style={{
+              fontWeight: "bold",
+              marginBottom: 15,
+              fontFamily: "Montserrat",
+            }}
+          >
+            Football App
+          </Typography>
+          <Typography
+            variant="subtitle2"
+            style={{
+              fontWeight: "bold",
+              fontFamily: "Montserrat",
+              color: "white",
+            }}
+          >
+            Keep track of information about football competitions
+          </Typography>
+        </div>
+      </Container>
+    </div>
+  );
 };
 
 export default Banner;
