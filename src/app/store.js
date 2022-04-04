95ee9fcd22ac5a1ff6174524707b5ae89e8bd71d
Andras Varga <vargaae@hotmail.com>
2022-04-04 03:01:30 +0200

    adding a slice reducer to the Redux store

--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 
+import { footballApi } from '../features/Api/FootballApi';
+
 export default configureStore({
     reducer: {
+        [footballApi.reducerPath]: footballApi.reducer,
     },
+    middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware()
+    .concat(footballApi.middleware)
 })
